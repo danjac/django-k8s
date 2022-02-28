649706db38043da3c00a0ef9f5d937b2046e0578
danjac <danjac2018@gmail.com>
2022-02-28 15:27:03 +0200

    Use skaffold

--- djk8s/urls.py
+++ djk8s/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.template.response import TemplateResponse
-
+from django.urls import path
 
 
 def health_check(request):
